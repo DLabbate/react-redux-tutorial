4832b5d49610d5db8d5746a323b44e781afdfdce
Domenic Labbate <domeniclabbate@gmail.com>
2022-10-22 17:27:03 -0400

    Remove Quote from App.tsx (starter code)

--- code/starter/src/App.tsx
+++ code/starter/src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import Input from "./components/Input";
-import Quote from "./components/Quote";
 import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <Quote />
       <Input />
       <TodoList />
     </div>
